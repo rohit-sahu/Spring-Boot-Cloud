2bd6631037df876d3b500e3562f13c96eda8158a
rohit-sahu <rohit.kumar@kuliza.com>
2020-01-01 14:15:02 +0530

    Added dynamic update functionality for pull the properties

--- git-config-client/src/main/java/com/springboot/gitconfigclient/controller/MessageController.java
+++ git-config-client/src/main/java/com/springboot/gitconfigclient/controller/MessageController.java
@@ -1,10 +1,12 @@
 package com.springboot.gitconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/api")
 public class MessageController {
