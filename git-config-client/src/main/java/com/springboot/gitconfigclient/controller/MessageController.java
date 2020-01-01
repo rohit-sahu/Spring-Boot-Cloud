95b6493fe9f9502a7abacd101c133a7c4dfdf849
rohit-sahu <rohit.kumar@kuliza.com>
2020-01-01 13:20:18 +0530

    Added Git Config server and Git Config Client

---
+++ git-config-client/src/main/java/com/springboot/gitconfigclient/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.springboot.gitconfigclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MessageController {
+
+    @Value("${message: Default}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getConfigMessage() {
+        return "";
+    }
+}
