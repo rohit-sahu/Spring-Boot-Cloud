4b82d026b9f9c3b771af889dc05bafe0586422f1
rohit-sahu <rohit.kumar@kuliza.com>
2020-03-13 12:15:42 +0530

    Hytrix config

---
+++ student-hystrix-service/src/test/java/com/springboot/studenthystrixservice/StudentHystrixServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studenthystrixservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHystrixServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
